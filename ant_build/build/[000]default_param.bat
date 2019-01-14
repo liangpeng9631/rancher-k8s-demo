14bbc21d1494013db5d4d47d6127898e15588c98
王海峰 <15000432144@163.com>
2019-01-14 22:49:30 +0800

    +

--- ant_build/build/[000]default_param.bat
+++ ant_build/build/[000]default_param.bat
@@ -8,6 +8,9 @@ rem cls
 echo "1.context->dev[default]"
 echo "2.context->test"
 echo "3.context->release"
+echo "4.context->wuhan"
+echo "5.context->dev_demo"
+echo "6.context->release_ingress"
 
 set /p context=please input [context] number:
 
@@ -22,14 +25,14 @@ echo "5.CPU->2048m"
 
 set /p cpu=please input [cpu] number:
 
-rem 内存信息
+rem 内存信息 堆栈固定比例100:25 仅JAVA
 rem cls
-echo "1.MEMORY->128Mi[default]"
-echo "2.MEMORY->256Mi"
-echo "3.MEMORY->512Mi"
-echo "4.MEMORY->768Mi"
-echo "5.MEMORY->1024Mi"
-echo "6.MEMORY->2048Mi"
+echo "1.MEMORY->128Mi  (heap96Mi   _ stack32Mi)[default]"
+echo "2.MEMORY->256Mi  (heap192Mi  _ stack64Mi)"
+echo "3.MEMORY->512Mi  (heap384Mi  _ stack128Mi)"
+echo "4.MEMORY->768Mi  (heap576Mi  _ stack192Mi)"
+echo "5.MEMORY->1024Mi (heap768Mi  _ stack256Mi)"
+echo "6.MEMORY->2048Mi (heap1536Mi _ stack512Mi)"
 
 set /p memory=please input [memory] number:
 
@@ -86,6 +89,18 @@ if "%context%"=="3" (
 set context=release
 )
 
+if "%context%"=="4" (
+set context=wuhan
+)
+
+if "%context%"=="5" (
+set context=dev_demo
+)
+
+if "%context%"=="6" (
+set context=release_ingress
+)
+
 rem CPU参数
 
 if "%cpu%"=="" (
@@ -116,30 +131,37 @@ rem MEMORY参数
 
 if "%memory%"=="" (
 set memory=128Mi
+set heap=96m
 )
 
 if "%memory%"=="1" (
 set memory=128Mi
+set heap=96m
 )
 
 if "%memory%"=="2" (
 set memory=256Mi
+set heap=192m
 )
 
 if "%memory%"=="3" (
 set memory=512Mi
+set heap=384m
 )
 
 if "%memory%"=="4" (
 set memory=768Mi
+set heap=576m
 )
 
 if "%memory%"=="5" (
 set memory=1024Mi
+set heap=768m
 )
 
 if "%memory%"=="6" (
 set memory=2048Mi
+set heap=1536m
 )
 
 rem 打印信息确认
