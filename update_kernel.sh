4502ad27f7a74cdbf2ce79eee9349202bb6e9bd4
王海峰 <15000432144@163.com>
2019-03-12 19:09:54 +0800

    新增优化后的docker交付脚本,增加内核升级 解决docker运行下偶发的被删除文件系统繁忙不能被回收 解决kernel:unregister_netdevice: waiting for lo to become free. Usage count = 1问题(暂时未遇到,传说4.15以前版本的内核都有这个问题)

---
+++ update_kernel.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+######################
+####升级内核至4.20############################################################################################################
+######################
+
+#安装wget用于下载远端安装包
+yum -y install wget
+
+#创建/data/目录
+mkdir -p /data/
+
+#进入所创建的目录
+cd /data/;
+
+#创建docker目录
+mkdir -p docker;
+
+#下载安装包
+wget http://docker-snake.cn-bj.ufileos.com/kernel-ml-4.20.13-1.el7.elrepo.x86_64.rpm
+wget http://docker-snake.cn-bj.ufileos.com/kernel-ml-devel-4.20.13-1.el7.elrepo.x86_64.rpm
+
+#通过安装更新内核
+rpm -ivh kernel-ml-devel-4.20.13-1.el7.elrepo.x86_64.rpm
+rpm -ivh kernel-ml-4.20.13-1.el7.elrepo.x86_64.rpm
+
+#修改内核启动顺序,默认启动的顺序应该为1,升级以后内核是往前面插入,为0（如果每次启动时需要手动选择哪个内核,该步骤可以省略）
+grub2-set-default  0 && grub2-mkconfig -o /etc/grub2.cfg
+
+#使用下面命令看看确认下是否启动默认内核指向上面安装的内核
+grubby --default-kernel
+
+#
+echo please reboot
