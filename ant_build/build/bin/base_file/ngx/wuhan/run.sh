36cd469ed3642e9b0999d5fa97b912b1a19122ab
王海峰 <15000432144@163.com>
2019-05-07 22:22:28 +0800

    编译优化

--- ant_build/build/bin/base_file/ngx/wuhan/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/data/nginx/sbin/nginx -g 'daemon off;' -c /data/nginx/conf/nginx.conf
--- ant_build/build/bin/base_file/php_fpm/dev_demo/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-/data/nginx/sbin/nginx -c /data/nginx/conf/nginx.conf &
-
-/data/php56/sbin/php-fpm
